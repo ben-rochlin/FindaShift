e4090f3aa7d1581c263d54a974d73ce04d4b1a69
ben-rochlin <brochlin91@gmail.com>
2020-08-14 04:07:21 +1000

    can't figure out how to get form to work

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,5 +1,6 @@
 class EmployeesController < ApplicationController
   before_action :set_employee, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, :except => [:index]
 
   # GET /employees
   # GET /employees.json
@@ -16,6 +17,7 @@ class EmployeesController < ApplicationController
   # GET /employees/new
   def new
     @employee = Employee.new
+    
   end
 
   # GET /employees/1/edit
