003215e9c5d303e2cf58e012398c70142dd20a17
ben-rochlin <brochlin91@gmail.com>
2020-08-26 20:22:17 +1000

    few more tweaks, also, found bug for inbox/messaging but no time to fix

--- app/views/managers/_manager.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! manager, :id, :name, :user_id, :created_at, :updated_at
-json.url manager_url(manager, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
 
 
   resources :jobs
-  resources :managers
   resources :employees
   resources :companies
   resources :job_applications
@@ -18,11 +17,11 @@ Rails.application.routes.draw do
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "test1/show"
-  get "test1/profile"
-  get "test1/jobs"
-  get "test1/new"
-  get "test2/show"
+  # get "test1/show"
+  # get "test1/profile"
+  # get "test1/jobs"
+  # get "test1/new"
+  # get "test2/show"
   # get "test2/listings", to: "test2#listings", as: "ownjobs"
   # get "test2/view/:id", to: "test2#view", as: "viewlisting"
 
